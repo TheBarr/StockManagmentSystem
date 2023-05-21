b3a9c48e72b9ab51966f0b6d199405dff0a1786a
Bartek <bartex600700@gmail.com>
2023-05-21 14:01:00 +0200

    Project

---
+++ StockManagmentSystem/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for StockManagmentSystem project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StockManagmentSystem.settings')
+
+application = get_asgi_application()
