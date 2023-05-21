a0b96b698500e917cdaf1c394dddc00a654b847e
Bartek <bartex600700@gmail.com>
2023-05-21 16:08:38 +0200

    Project

--- StockManagmentSystem/urls.py
+++ StockManagmentSystem/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from stockmg import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
--- stockmg/views.py
+++ stockmg/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    title = 'Welcome: This is the Home Page'
+    form = 'Siema'
+    context = {
+        "title": title,
+        "form": form,
+    }
+    return render(request, "home.html", context)
